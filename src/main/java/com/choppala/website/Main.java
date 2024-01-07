6a669ef7c3ea77439995154d21fa15f27e41458c
Alexander Alvonellos <alexander.alvonellos@gmail.com>
2024-01-06 21:07:55 -0500

    quick code cleanup

--- src/main/java/com/choppala/website/Main.java
+++ src/main/java/com/choppala/website/Main.java
@@ -14,39 +14,16 @@ import java.util.Map;
 @SpringBootApplication
 @Controller
 public class Main {
-    private final DataSource dataSource;
-
-    @Autowired
-    public Main(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
-    @GetMapping("/database")
-    String database(Map<String, Object> model) {
-        try (Connection connection = dataSource.getConnection()) {
-            final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
-
-            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
-            final var output = new ArrayList<>();
-            while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
-            }
-
-            model.put("records", output);
-            return "database";
-
-        } catch (Throwable t) {
-            model.put("message", t.getMessage());
-            return "error";
-        }
+    @GetMapping("/about")
+    public String about() {
+        return "about";
     }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
